0aff8098abb4a4a92370f8c007b80cc4da44bfd5
Shuichi.K <syuichisyuichi7010@icloud.com>
2020-06-19 21:18:59 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_17_025720) do
+ActiveRecord::Schema.define(version: 2020_06_19_112825) do
 
   create_table "deliveryaddresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "family_name", null: false
@@ -27,6 +27,18 @@ ActiveRecord::Schema.define(version: 2020_06_17_025720) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_deliveryaddresses_on_user_id"
+  end
+
+  create_table "profieladdresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "postal_code"
+    t.string "prefectures"
+    t.string "city"
+    t.string "address"
+    t.string "building"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profieladdresses_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -55,5 +67,5 @@ ActiveRecord::Schema.define(version: 2020_06_17_025720) do
   end
 
   add_foreign_key "deliveryaddresses", "users"
-
+  add_foreign_key "profieladdresses", "users"
 end
