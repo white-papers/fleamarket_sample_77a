8142de36f26f8743e9f8d446f5088ddeded00e25
Shuichi.K <syuichisyuichi7010@icloud.com>
2020-06-19 21:22:27 +0900

    change routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     get 'deliveryaddresses',to: 'users/registrations#new_deliveryaddresses'
     post 'deliveryaddresses',to: 'users/registrations#create_deliveryaddresses'
   end
-  root 'item#index'
+  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
