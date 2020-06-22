46a2a30286158cf6365756e53e249b5148cec86f
AsukaFukaumi <asuarks@outlook.com>
2020-06-22 11:26:17 +0900

    商品出品ページのビュー作成
    resources :products に new を追加
    products_controller に new アクションを追加
    new.html.haml を作成
    products_new.scss を作成
    products_new.js を作成
    app/assets/images 配下に仮置き画像配置

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   end
   root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products, only: [:new]
 end
