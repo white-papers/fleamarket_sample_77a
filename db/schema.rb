d57695b66dd7885d157a76c79221051c3f6502eb
AsukaFukaumi <asuarks@outlook.com>
2020-06-24 19:32:22 +0900

    login修正
    ログイン画面のエラー修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_062342) do
+ActiveRecord::Schema.define(version: 2020_06_24_095504) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 2020_06_24_062342) do
     t.string "family_name_kana", null: false
     t.string "given_name_kana", null: false
     t.string "postal_code", null: false
-    t.string "prefectures", null: false
     t.string "city", null: false
     t.string "address", null: false
     t.string "building"
@@ -34,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_06_24_062342) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "prefecture_id"
     t.index ["user_id"], name: "index_deliveryaddresses_on_user_id"
   end
 
