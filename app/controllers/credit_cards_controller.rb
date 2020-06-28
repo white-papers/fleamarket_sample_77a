6310b3e4f023feab85e7b0925f1e335dd65cc24e
fukyann <fukuyamayade@gmail.com>
2020-06-28 15:53:35 +0900

    .DS_Storeをgitから除外、不要な記述を削除

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -48,8 +48,6 @@ class CreditCardsController < ApplicationController
       @card_brand = @customer_card.brand
       case @card_brand
       when "Visa"
-        # 例えば、Pay.jpからとってきたカード情報の、ブランドが"Visa"だった場合は返り値として
-        # (画像として登録されている)Visa.pngを返す
         @card_src = "visa.png"
       when "JCB"
         @card_src = "jcb.png"
@@ -63,7 +61,6 @@ class CreditCardsController < ApplicationController
         @card_src = "discover.png"
       end
 
-      #  viewの記述を簡略化
       ## 有効期限'月'を定義
       @exp_month = @customer_card.exp_month.to_s
       ## 有効期限'年'を定義
@@ -73,24 +70,17 @@ class CreditCardsController < ApplicationController
   
   
   def destroy
-    # ログイン中のユーザーのクレジットカード登録の有無を判断
     @card = CreditCard.find_by(user_id: current_user.id)
     if @card.blank?
-      # 未登録なら新規登録画面に
       redirect_to action: "new"
     else
-      # 前前回credentials.yml.encに記載したAPI秘密鍵を呼び出します。
       Payjp.api_key = ENV["PAYJP_ACCESS_KEY"]
-      # ログインユーザーのクレジットカード情報からPay.jpに登録されているカスタマー情報を引き出す
       customer = Payjp::Customer.retrieve(@card.customer_id)
-      # そのカスタマー情報を消す
+      # カスタマー情報を消す
       customer.delete
       @card.delete
       # 削除が完了しているか判断
       if @card.destroy
-      # 削除完了していればdestroyのビューに移行
-      # destroyビューを作るのが面倒であれば、flashメッセージを入れてトップページやマイページに飛ばしてもOK
-
       else
         # 削除されなかった場合flashメッセージを表示させて、showのビューに移行
         redirect_to credit_card_path(current_user.id), alert: "削除できませんでした。"
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,87 @@
 class OrdersController < ApplicationController
-  def new
-  end
+  
+  # require "payjp"
+
+  # def new
+  #   # 購入する商品を引っ張ってきます。
+  #   @product = Product.find(params[:product_id])
+  #   # 商品ごとに複数枚写真を登録できるので、一応全部持ってきておきます。
+  #   @images = @product.images.all
+
+  #   if user_signed_in?
+  #     @user = current_user
+  #     # クレジットカードが登録されているか確認
+  #     if @user.credit_card.present?
+  #       Payjp.api_key = ENV["PAYJP_ACCESS_KEY"]
+  #       # ログインユーザーのクレジットカード情報を引っ張ってきます。
+  #       @card = CreditCard.find_by(user_id: current_user.id)
+  #       # ログインユーザーのクレジットカード情報からPay.jpに登録されているカスタマー情報を引き出す
+  #       customer = Payjp::Customer.retrieve(@card.customer_id)
+  #       # カスタマー情報からカードの情報を引き出す
+  #       @customer_card = customer.cards.retrieve(@card.card_id)
+
+  #       ##カードのアイコン表示のための定義づけ
+  #       @card_brand = @customer_card.brand
+  #       case @card_brand
+  #       when "Visa"
+  #         # (画像として登録されている)Visa.pngを返す
+  #         @card_src = "visa.gif"
+  #       when "JCB"
+  #         @card_src = "jcb.gif"
+  #       when "MasterCard"
+  #         @card_src = "master.png"
+  #       when "American Express"
+  #         @card_src = "amex.gif"
+  #       when "Diners Club"
+  #         @card_src = "diners.gif"
+  #       when "Discover"
+  #         @card_src = "discover.gif"
+  #       end
+  
+     
+  #       @exp_month = @customer_card.exp_month.to_s
+  #       @exp_year = @customer_card.exp_year.to_s.slice(2,3)
+  #     else
+  #     end
+  #   else
+  #     redirect_to user_session_path, alert: "ログインしてください"
+  #   end
+  # end
+
+  # def pay
+  #   @product = Product.find(params[:product_id])
+  #   @images = @product.images.all
+
+  #   # 購入テーブル登録ずみ商品は２重で購入されないようにする
+  #   if @product.purchase.present?
+  #     redirect_to product_path(@product.id), alert: "売り切れています。"
+  #   else
+  #     # 同時に2人が同時に購入し、二重で購入処理がされることを防ぐための記述
+  #     @product.with_lock do
+  #       if current_user.credit_card.present?
+  #         # ログインユーザーがクレジットカード登録済みの場合の処理
+  #         @card = CreditCard.find_by(user_id: current_user.id)
+  #         Payjp.api_key = ENV["PAYJP_ACCESS_KEY"]
+  #         #登録したカードでの、クレジットカード決済処理
+  #         charge = Payjp::Charge.create(
+  #         amount_of_money: @product.price,
+  #         customer: Payjp::Customer.retrieve(@card.customer_id),
+  #         currency: 'jpy'
+  #         )
+  #       else
+  #         # ログインユーザーがクレジットカード登録されていない場合(Checkout機能による処理を行います)
+  #         # APIの「Checkout」ライブラリによる決済処理の記述
+  #         Payjp::Charge.create(
+  #         amount_of_money: @product.price,
+  #         card: params['payjp-token'], # フォームを送信すると作成・送信されてくるトークン
+  #         currency: 'jpy'
+  #         )
+  #       end
+  #     #購入テーブルに登録処理(今回の実装では言及しませんが一応、、、)
+  #     @purchase = Purchase.create(buyer_id: current_user.id, product_id: params[:product_id])
+  #     end
+  #   end
+  # end
+
+
 end
