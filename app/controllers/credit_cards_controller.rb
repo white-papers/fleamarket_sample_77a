bd59e4b9189a9af16383749b8e48cc9281ee1e22
fukyann <fukuyamayade@gmail.com>
2020-06-29 12:45:45 +0900

    行間の間違いを変更

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -9,7 +9,6 @@ class CreditCardsController < ApplicationController
   end
   
   def create #payjpとCardのデータベース作成
-   
     Payjp.api_key = ENV["PAYJP_ACCESS_KEY"]
     #保管した顧客IDでpayjpから情報取得
     if params['payjp-token'].blank?
@@ -23,7 +22,7 @@ class CreditCardsController < ApplicationController
       @card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
      
       if @card.save
-        redirect_to  credit_card_path(current_user.id)
+        redirect_to credit_card_path(current_user.id)
       else
         new_credit_card_path
       end
@@ -86,8 +85,5 @@ class CreditCardsController < ApplicationController
       end
     end
   end
-
-   
-
 end
 
