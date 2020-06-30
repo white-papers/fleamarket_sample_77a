97486ad01504ab240f5e0de12facfd38a65eedb1
AsukaFukaumi <asuarks@outlook.com>
2020-06-30 21:33:18 +0900

    カテゴリ別商品ページにリンクを追加
    カテゴリ別商品ページにリンクを追加
    コードを見やすく修正
    出品ページの画像リンクをフルパスに変更

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,6 +7,7 @@ before_action :set_category, only: [:show]
 
   def show
     @products = @category.set_products
+    # .where(buyer_id: nil) を消すと購入済も表示
     @products = @products.where(buyer_id: nil).order("created_at DESC")
   end  
 
@@ -14,6 +15,14 @@ before_action :set_category, only: [:show]
   def set_category
     # カテゴリーidを元に,クリックされたカテゴリーのレコードを取得
     @category = Category.find(params[:id])
+
+    # 一階層下のカテゴリーがある場合はそれを格納
+    if @category.has_children?
+      @category_links = @category.children
+    # 無い場合は同じ階層のカテゴリーを格納
+    else
+      @category_links = @category.siblings
+    end
   end
 
 end
