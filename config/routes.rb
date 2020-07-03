eb5939bdd12aa054db1e58909e3cf816d9b0f5e7
fukyann <fukuyamayade@gmail.com>
2020-07-03 18:54:04 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     get 'deliveryaddresses',to: 'users/registrations#new_deliveryaddresses'
     post 'deliveryaddresses',to: 'users/registrations#create_deliveryaddresses'
   end
+<<<<<<< Updated upstream
 
   root 'products#index'
 
@@ -44,4 +45,8 @@ Rails.application.routes.draw do
     resources :favorites , only: [:index, :create, :destroy]
   end
 
+=======
+  root 'products#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+>>>>>>> Stashed changes
 end
