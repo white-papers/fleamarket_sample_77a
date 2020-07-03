1b7defe95d46adb4577d555a41545ffbfc1b0abf
Shuichi.K <syuichisyuichi7010@icloud.com>
2020-07-03 19:42:14 +0900

    change colum of orders_table

--- db/migrate/20200625062829_create_orders.rb
+++ db/migrate/20200625062829_create_orders.rb
@@ -1,7 +1,6 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-      t.references :product,                  foreign_key: true
       t.references :deliveryaddress,          foreign_key: true
       t.references :credit_card,              foreign_key: true
       t.references :exhibitor,                foreign_key: {to_table: :users}
