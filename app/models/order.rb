c54448f1f868eb4966723163e86c4ff0d0259438
Shuichi.K <syuichisyuichi7010@icloud.com>
2020-07-03 14:43:58 +0900

    modify production.rb to fix deproy

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,8 @@
 class Order < ApplicationRecord
   has_one :deliveryaddress
-  belongs_to :exhibitor, class_name: 'User',foreign_key:'exhibitor_id', optional: true
-  belongs_to :buyer, class_name: 'User',foreign_key:'buyer_id', optional: true
-  belongs_to :product, optional: true
-  belongs_to :credit_card, optional: true
+  belongs_to :exhibitor, class_name: 'User',foreign_key:'exhibitor_id'
+  belongs_to :buyer, class_name: 'User',foreign_key:'buyer_id'
+  belongs_to :product
+  belongs_to :credit_card
 
 end
