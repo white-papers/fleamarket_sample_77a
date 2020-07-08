693cc6d2f7629a5245b9849d3d36270eb3ca503f
AsukaFukaumi <asuarks@outlook.com>
2020-07-08 19:07:19 +0900

    前へ・次へボタンのリンク修正
    ・前後にデータが存在しない場合は、遷移のボタンが表示されない
    ・削除されたデータは飛ばして遷移する

--- app/models/product.rb
+++ app/models/product.rb
@@ -20,4 +20,12 @@ class Product < ApplicationRecord
     Product.where(['name LIKE ?', "%#{search}%"])
   end
 
+  def previous
+    Product.where("id < ?", self.id).order("id DESC").first
+  end
+
+  def next
+    Product.where("id > ?", self.id).order("id ASC").first
+  end
+
 end
